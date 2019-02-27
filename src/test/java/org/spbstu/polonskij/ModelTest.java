72563ca6659c56d734f017124269e10e40211732
kryak <polonskim@outlook.com>
2019-02-27 18:20:27 +0300

    commit

--- src/test/java/org/spbstu/polonskij/ModelTest.java
+++ src/test/java/org/spbstu/polonskij/ModelTest.java
@@ -2,6 +2,16 @@ package org.spbstu.polonskij;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
@@ -13,7 +23,8 @@ public class ModelTest {
             String path = "src/test/resources/input/nonexistent_file";
             new SuffixTree(path);
             assertTrue(false);
-        } catch (Exception ex){}
+        } catch (Exception ex) {
+        }
         assertTrue(true);
     }
 
@@ -23,16 +34,17 @@ public class ModelTest {
             String path = "src/test/resources/input/file_1";
             new SuffixTree(path);
             assertTrue(false);
-        } catch (Exception ex){}
+        } catch (Exception ex) {
+        }
         assertTrue(true);
     }
 
     @Test
     public void searchSub() {
-            String path = "src/test/resources/input/file_2";
-            String result = "Result of search: [o, ve you, u]";
-            SuffixTree suffixTree = new SuffixTree(path);
-            assertEquals(result, suffixTree.searchSubstring("src/test/resources/input/sub_for_file_2"));
+        String path = "src/test/resources/input/file_2";
+        String result = "Result of search: [o, ve you, u]";
+        SuffixTree suffixTree = new SuffixTree(path);
+        assertEquals(result, suffixTree.searchSubstring("src/test/resources/input/sub_for_file_2"));
     }
 
     @Test
@@ -40,5 +52,92 @@ public class ModelTest {
         String path = "src/test/resources/input/file_2";
         String result = "Longest repeated substring: o";
         assertEquals(result, new SuffixTree(path).longRepSubstring());
+    }
+
+    private static boolean equalFiles(String expectedFileName,
+                                      String resultFileName) {
+        boolean equal;
+        BufferedReader bExp;
+        BufferedReader bRes;
+        String expLine;
+        String resLine;
+
+        equal = false;
+        bExp = null;
+        bRes = null;
+
+        try {
+            bExp = new BufferedReader(new FileReader(expectedFileName));
+            bRes = new BufferedReader(new FileReader(resultFileName));
+
+            if ((bExp != null) && (bRes != null)) {
+                expLine = bExp.readLine();
+                resLine = bRes.readLine();
+
+                equal = ((expLine == null) && (resLine == null)) || ((expLine != null) && expLine.equals(resLine));
+
+                while (equal && expLine != null) {
+                    expLine = bExp.readLine();
+                    resLine = bRes.readLine();
+                    equal = expLine.equals(resLine);
+                }
+            }
+        } catch (Exception e) {
+
+        } finally {
+            try {
+                if (bExp != null) {
+                    bExp.close();
+                }
+                if (bRes != null) {
+                    bRes.close();
+                }
+            } catch (Exception e) {
+            }
+
+        }
+
+        return equal;
+
+    }
+
+    private boolean equalsFile(List<File> expected, List<File> actual) throws IOException {
+        boolean answ = true;
+        int counter = 0;
+        if (expected.size() != actual.size()) answ = false;
+        for (File files : expected) {
+            Scanner scannerExpected = new Scanner(files);
+            Scanner scannerActual = new Scanner(actual.get(counter));
+            while (scannerActual.hasNextLine()) {
+                if (!scannerExpected.hasNextLine()) {
+                    answ = false;
+                    break;
+                }
+                String strExpected = scannerExpected.nextLine();
+                String strActual = scannerActual.nextLine();
+                if (!strExpected.equals(strActual)) {
+                    answ = false;
+                }
+            }
+            if (!answ) {
+                break;
+            } else {
+                counter++;
+            }
+        }
+        return answ;
+    }
+
+
+    @Test
+    public void buildTree() throws IOException {
+        String act = "src/test/resources/input/file_2";
+        File tr = new SuffixTree(act).visualize();
+        Path out = Paths.get("C:\\Users\\Asus\\IdeaProjects\\example\\result.txt");
+        List<File> expected = new ArrayList<File>();
+        List<File> actual = new ArrayList<File>();
+        actual.add(new File(String.valueOf(out)));
+        expected.add(new File("src\\test\\resources\\input\\tree"));
+        assertTrue(equalsFile(expected, actual));
     }
 }
